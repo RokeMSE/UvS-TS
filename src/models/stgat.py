886d3c3bed0d3d42df9640e9701a006190dee92f
DucHung0109 <ndh.01.09.2004@gmail.com>
2025-11-23 18:17:05 +0700

    fix stgat

--- src/models/stgat.py
+++ src/models/stgat.py
@@ -1,66 +1,26 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+import torch.nn as nn
 
-class GraphAttentionLayer(nn.Module):
-    def __init__(self, in_features: int, out_features: int, n_heads: int,
-                 is_concat: bool = True,
-                 dropout: float = 0.6,
-                 leaky_relu_negative_slope: float = 0.2):
-        super().__init__()
-
-        self.is_concat = is_concat
-        self.n_heads = n_heads
-
-        if is_concat:
-            assert out_features % n_heads == 0
-            self.n_hidden = out_features // n_heads
-        else:
-            self.n_hidden = out_features
-        self.linear = nn.Linear(in_features, self.n_hidden * n_heads, bias=False)
-        self.attn = nn.Linear(self.n_hidden * 2, 1, bias=False)
-        self.activation = nn.LeakyReLU(negative_slope=leaky_relu_negative_slope)
-        self.softmax = nn.Softmax(dim=1)
-        self.dropout = nn.Dropout(dropout)
-    def forward(self, h, adj_mat):
-        n_nodes = h.shape[0]
-        g = self.linear(h).view(n_nodes, self.n_heads, self.n_hidden)
-
-        g_i = g.unsqueeze(1)      # (N,1,h,n_hidden)
-        g_j = g.unsqueeze(0)      # (1,N,h,n_hidden)
-        g_concat = torch.cat([g_i.expand(n_nodes,n_nodes,-1,-1), g_j.expand(n_nodes,n_nodes,-1,-1)], dim=-1)
-        
-        e = self.activation(self.attn(g_concat)).squeeze(-1)
-        assert adj_mat.shape[0] == 1 or adj_mat.shape[0] == n_nodes
-        assert adj_mat.shape[1] == 1 or adj_mat.shape[1] == n_nodes
-        assert adj_mat.shape[2] == 1 or adj_mat.shape[2] == self.n_heads
-        e = e.masked_fill(adj_mat == 0, float('-inf'))
-        a = self.softmax(e)
-        a = self.dropout(a)
-        attn_res = torch.einsum('ijh,jhf->ihf', a, g)
-        if self.is_concat:
-            return attn_res.reshape(n_nodes, self.n_heads * self.n_hidden)
-        else:
-            return attn_res.mean(dim=1)
-
-
-class STGAT(nn.Module):
-    def __init__(self, num_nodes, nums_feature_input, nums_step_in, nums_step_out, nums_feature_output=3, n_heads=8):
-        super().__init__()
-        self.hidden_dim = 32
+class STGAT(torch.nn.Module):
+    def __init__(self, num_nodes, nums_step_in, nums_step_out, nums_feature_input, nums_feature_output, n_heads=8, dropout=0.0):
+        """
+        Initialize the ST-GAT model
+        :param in_channels Number of input channels
+        :param out_channels Number of output channels
+        :param n_nodes Number of nodes in the graph
+        :param heads Number of attention heads to use in graph
+        :param dropout Dropout probability on output of Graph Attention Network
+        """
+        super(STGAT, self).__init__()
         self.num_timesteps_output = nums_step_out
-        self.num_features_output = nums_feature_output
-        # spatial
-        self.gat = GraphAttentionLayer(nums_feature_input, self.hidden_dim, n_heads, dropout=0)
-
-        # temporal: stacked LSTM
-        self.lstm = nn.LSTM(
-            input_size=self.hidden_dim,
-            hidden_size=self.hidden_dim,
-            num_layers=2,     # 2 layers LSTM
-            batch_first=True
-        )
-        self.fc = nn.Linear(self.hidden_dim, nums_feature_output * nums_step_out)
+        self.heads = n_heads
+        self.dropout = dropout
+        self.n_nodes = num_nodes
+        self.edge_index = None
+        self.n_preds = 9
+        self.hidden_dim = 32
 
         self.config = {
             "num_nodes": num_nodes,
@@ -71,22 +31,47 @@ class STGAT(nn.Module):
             "n_head": n_heads
         }
 
+        # single graph attentional layer with 8 attention heads
+        self.gat = GATConv(in_channels=nums_feature_input, out_channels=self.hidden_dim,
+            heads=n_heads, dropout=0, concat=False)
+        
+        self.lstm = nn.LSTM(
+            input_size=self.hidden_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=2,     # 2 layers LSTM
+            batch_first=True
+        )
+        self.fc = nn.Linear(self.hidden_dim, nums_feature_output * nums_step_out)
+
+    def process(self, W):
+        edge_index = torch.nonzero(W, as_tuple=False).t() #(2, num_edges)
+        self.edge_index = edge_index.to(device=W.device)
+        
     def forward(self, A_hat, X):
-
-        B, N, T_in, F = X.shape
-
+        """
+        Forward pass of the ST-GAT model
+        :param data Data to make a pass on
+        :param device Device to operate on
+        """
+        if self.edge_index != None:
+            edge_index = self.edge_index
+        else:
+            self.process(A_hat)
+            edge_index = self.edge_index
+        
+        B, N, T_in, F = X.shape  
+        X_reshaped = X.permute(2, 0, 1, 3)  # (T, B, N, F)
+        X_reshaped = X_reshaped.reshape(T_in, B*N, F)  # (T, B*N, F)
         outs = []
         for t in range(T_in):
-            h_t = []
-            for b in range(B):
-                h_t.append(self.gat(X[b, :, t, :], A_hat.unsqueeze(-1)))
-            h_t = torch.stack(h_t, dim=0)  # (B, N, hidden_dim)
-            outs.append(h_t.unsqueeze(2))   # add time dim
+            x_t = X_reshaped[t]
+            h_t = self.gat(x_t, edge_index)
+            h_t = h_t.reshape(B, N, -1)
+            outs.append(h_t.unsqueeze(2))
 
         # stack timesteps → (B, N, T_in, hidden_dim)
         outs = torch.cat(outs, dim=2)
 
-        # merge batch & nodes for LSTM
         lstm_in = outs.reshape(B * N, T_in, self.hidden_dim)
         lstm_out, _ = self.lstm(lstm_in)
         h_last = lstm_out[:, -1, :]
